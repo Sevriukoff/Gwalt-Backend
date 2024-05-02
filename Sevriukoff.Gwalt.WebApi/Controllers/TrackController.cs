4d702c191494dff552e31fa95463520aea66df68
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-05-02 17:33:23 +0700

    Init proj

---
+++ Sevriukoff.Gwalt.WebApi/Controllers/TrackController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sevriukoff.Gwalt.WebApi.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class TrackController : ControllerBase
+{
+
+}
---
+++ Sevriukoff.Gwalt.WebApi/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sevriukoff.Gwalt.WebApi.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class UserController : ControllerBase
+{
+    
+}
