7d2ffad98cabffbe547274b8bef96e8bf735257f
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-05-05 11:50:55 +0700

    Configured dependencies for FileService. Added a controller for file operations.

---
+++ Sevriukoff.Gwalt.WebApi/Controllers/FileController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sevriukoff.Gwalt.Application.Interfaces;
+
+namespace Sevriukoff.Gwalt.WebApi.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class FileController : ControllerBase
+{
+    private readonly IFileService _fileService;
+    
+    public FileController(IFileService fileService)
+    {
+        _fileService = fileService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post()
+    {
+        try
+        {
+            var contentType = Request.ContentType;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await Request.Body.CopyToAsync(memoryStream);
+                
+                var fileId = await _fileService.UploadImageAsync(memoryStream, contentType);
+
+                return CreatedAtAction(nameof(Get),new { id = fileId }, fileId);
+            }
+        }
+        catch (Exception ex)
+        {
+            return BadRequest("Error processing binary file: " + ex.Message);
+        }
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete()
+    {
+        return Ok();
+    }
+}
--- Sevriukoff.Gwalt.WebApi/Program.cs
+++ Sevriukoff.Gwalt.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Amazon.S3;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Sevriukoff.Gwalt.Application.Interfaces;
 using Sevriukoff.Gwalt.Application.Services;
@@ -6,6 +9,8 @@ using Sevriukoff.Gwalt.Infrastructure.Interfaces;
 using Sevriukoff.Gwalt.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//TODO: Add Autofac
 
 builder.Services.AddDbContext<DataDbContext>(options =>
 {
@@ -17,6 +22,20 @@ builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddScoped<ITrackRepository, TrackRepository>();
 builder.Services.AddScoped<ITrackService, TrackService>();
+
+builder.Services.AddScoped<IFileService, FileService>();
+
+builder.Services.AddScoped<IAmazonS3, AmazonS3Client>(provider =>
+{
+    var config = new AmazonS3Config
+    {
+        ServiceURL = builder.Configuration["YandexStorageConfig:ServiceURL"],
+    };
+    
+    return new AmazonS3Client(config);
+});
+
+builder.Services.AddScoped<IFileStorage, YandexStorage>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
