19c42336f179cc1e824136932ebacc2da185e475
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-05-20 15:41:19 +0700

    Added Autofac for dependency injection.

--- Sevriukoff.Gwalt.WebApi/Program.cs
+++ Sevriukoff.Gwalt.WebApi/Program.cs
@@ -1,6 +1,8 @@
 using System.Text;
 using System.Text.Json.Serialization;
 using Amazon.S3;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -11,11 +13,51 @@ using Sevriukoff.Gwalt.Application.Services;
 using Sevriukoff.Gwalt.Infrastructure;
 using Sevriukoff.Gwalt.Infrastructure.Interfaces;
 using Sevriukoff.Gwalt.Infrastructure.Repositories;
+using Sevriukoff.Gwalt.WebApi.Common.Attributes;
 using Sevriukoff.Gwalt.WebApi.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtBearerConfig"));
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+var r = builder.Host.ConfigureContainer<ContainerBuilder>((context, containerBuilder) =>
+{
+    containerBuilder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerLifetimeScope();
+    containerBuilder.RegisterType<UserService>().As<IUserService>().InstancePerLifetimeScope();
+
+    containerBuilder.RegisterType<TrackRepository>().As<ITrackRepository>().InstancePerLifetimeScope();
+    containerBuilder.RegisterType<TrackService>().As<ITrackService>().InstancePerLifetimeScope();
+    
+    containerBuilder.RegisterType<AlbumRepository>().As<IAlbumRepository>().InstancePerLifetimeScope();
+    containerBuilder.RegisterType<AlbumService>().As<IAlbumService>().InstancePerLifetimeScope();
+    
+
+    containerBuilder.RegisterType<CommentRepository>().As<ICommentRepository>().InstancePerLifetimeScope();
+    containerBuilder.RegisterType<CommentService>().As<ICommentService>().InstancePerLifetimeScope();
+
+    containerBuilder.RegisterType<AuthService>().As<IAuthService>().InstancePerLifetimeScope();
+
+    containerBuilder.RegisterType<JwtHelper>().InstancePerLifetimeScope();
+    containerBuilder.RegisterType<PasswordHasher>().InstancePerLifetimeScope();
+
+    containerBuilder.RegisterType<FileService>().As<IFileService>().InstancePerLifetimeScope();
+
+    containerBuilder.Register<IAmazonS3>(context =>
+    {
+        var config = new AmazonS3Config
+        {
+            ServiceURL = builder.Configuration["YandexStorageConfig:ServiceURL"],
+        };
+        
+        return new AmazonS3Client(config);
+    }).InstancePerLifetimeScope(); 
+
+    containerBuilder.RegisterType<YandexStorage>().As<IFileStorage>().InstancePerLifetimeScope();
+});
+
+builder.Services.AddScoped<JwtHelper>();
+
 builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
 builder.Services.Configure<YandexStorageConfig>(builder.Configuration.GetSection("YandexStorageConfig"));
 
@@ -34,9 +76,11 @@ builder.Services.AddAuthentication(opt =>
         ValidateLifetime = true,
         ValidIssuer = builder.Configuration["JwtBearerConfig:Issuer"],
         ValidAudience = builder.Configuration["JwtBearerConfig:Audience"],
+        ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
+        ValidAudience = builder.Configuration["JwtConfig:Audience"],
         ValidateIssuerSigningKey = true,
         IssuerSigningKey =
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtBearerConfig:SecretKey"]))
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtConfig:SecretKey"]))
     };
 
     opt.Events = new JwtBearerEvents
@@ -83,34 +127,10 @@ builder.Services.AddStackExchangeRedisCache(opt =>
     opt.InstanceName = "Gwalt";
 });
 
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-
-builder.Services.AddScoped<ITrackRepository, TrackRepository>();
-builder.Services.AddScoped<ITrackService, TrackService>();
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-builder.Services.AddScoped<JwtHelper>();
-builder.Services.AddScoped<PasswordHasher>();
-
-builder.Services.AddScoped<IFileService, FileService>();
-builder.Services.AddScoped<IAmazonS3, AmazonS3Client>(provider =>
-{
-    var config = new AmazonS3Config
-    {
-        ServiceURL = builder.Configuration["YandexStorageConfig:ServiceURL"],
-    };
-    
-    return new AmazonS3Client(config);
-});
-
-builder.Services.AddScoped<IFileStorage, YandexStorage>();
 builder.Services.AddAutoMapper(typeof(ApplicationMappingProfile), typeof(PresentationMappingProfile));
 
 #endregion
 
-builder.Services.AddControllers()
 var provider = builder.Services.BuildServiceProvider();
 
 builder.Services.AddControllers(opt =>
