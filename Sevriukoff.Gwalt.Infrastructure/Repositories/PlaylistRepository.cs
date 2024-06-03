a05b2151983193b120cc028421003559738d8661
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-06-03 15:43:16 +0700

    Added basic implementation of album repository. Included method in TrackRepository for adding a new track.

---
+++ Sevriukoff.Gwalt.Infrastructure/Repositories/PlaylistRepository.cs
@@ -0,0 +1,10 @@
+﻿using Sevriukoff.Gwalt.Infrastructure.Base;
+using Sevriukoff.Gwalt.Infrastructure.Entities;
+using Sevriukoff.Gwalt.Infrastructure.Interfaces;
+
+namespace Sevriukoff.Gwalt.Infrastructure.Repositories;
+
+public class PlaylistRepository : BaseRepository<Playlist>, IPlaylistRepository
+{
+    public PlaylistRepository(DataDbContext context) : base(context) { }
+}
--- Sevriukoff.Gwalt.Infrastructure/Repositories/TrackRepository.cs
+++ Sevriukoff.Gwalt.Infrastructure/Repositories/TrackRepository.cs
@@ -1,4 +1,5 @@
-﻿using Sevriukoff.Gwalt.Infrastructure.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using Sevriukoff.Gwalt.Infrastructure.Base;
 using Sevriukoff.Gwalt.Infrastructure.Entities;
 using Sevriukoff.Gwalt.Infrastructure.Interfaces;
 
@@ -7,4 +8,14 @@ namespace Sevriukoff.Gwalt.Infrastructure.Repositories;
 public class TrackRepository : BaseRepository<Track>, ITrackRepository
 {
     public TrackRepository(DataDbContext context) : base(context) { }
+
+    public override async Task<int> AddAsync(Track entity)
+    {
+        foreach (var genre in entity.Genres)
+        {
+            Context.Entry(genre).State = EntityState.Unchanged;
+        }
+        
+        return await base.AddAsync(entity);
+    }
 }
